89fd5605fe9aaf010288ebefe4f6c329581c27cf
SAJAD AHMAD RATHER <46589981+SajadAHMAD1@users.noreply.github.com>
2019-11-22 15:27:19 +0530

    Update initializationSSA.m

--- initializationSSA.m
+++ initializationSSA.m
@@ -1,21 +1,4 @@
-%_________________________________________________________________________________
-%  Salp Swarm Algorithm (SSA) source codes version 1.0
-%
-%  Developed in MATLAB R2016a
-%
-%  Author and programmer: Seyedali Mirjalili
-%
-%         e-Mail: ali.mirjalili@gmail.com
-%                 seyedali.mirjalili@griffithuni.edu.au
-%
-%       Homepage: http://www.alimirjalili.com
-%
-%   Main paper:
-%   S. Mirjalili, A.H. Gandomi, S.Z. Mirjalili, S. Saremi, H. Faris, S.M. Mirjalili,
-%   Salp Swarm Algorithm: A bio-inspired optimizer for engineering design problems
-%   Advances in Engineering Software
-%   DOI: http://dx.doi.org/10.1016/j.advengsoft.2017.07.002
-%____________________________________________________________________________________
+__________________________________________________________
 
 
 % This function initialize the first population of search agents
