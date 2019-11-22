352fd8ee117a6517a5e0665d7445ecc88f846743
SAJAD AHMAD RATHER <46589981+SajadAHMAD1@users.noreply.github.com>
2019-11-22 15:26:27 +0530

    Update initializationGWO.m

--- initializationGWO.m
+++ initializationGWO.m
@@ -1,21 +1,4 @@
-%___________________________________________________________________%
-%  Grey Wolf Optimizer (GWO) source codes version 1.0               %
-%                                                                   %
-%  Developed in MATLAB R2011b(7.13)                                 %
-%                                                                   %
-%  Author and programmer: Seyedali Mirjalili                        %
-%                                                                   %
-%         e-Mail: ali.mirjalili@gmail.com                           %
-%                 seyedali.mirjalili@griffithuni.edu.au             %
-%                                                                   %
-%       Homepage: http://www.alimirjalili.com                       %
-%                                                                   %
-%   Main paper: S. Mirjalili, S. M. Mirjalili, A. Lewis             %
-%               Grey Wolf Optimizer, Advances in Engineering        %
-%               Software , in press,                                %
-%               DOI: 10.1016/j.advengsoft.2013.12.007               %
-%                                                                   %
-%___________________________________________________________________%
+
 
 % This function initialize the first population of search agents
 function Positions=initializationGWO(N,dim,up,down)
